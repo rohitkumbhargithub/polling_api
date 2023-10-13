7947fe3f23d27eb2524bc4eb0e084aca24ee5ba2
Rohit Kumbhar <rohitkumbhar956@gmail.com>
2023-10-13 17:41:07 +0530

    First commit: create the directory and setup express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(err);
+    }
+    console.log(`Server running at ${port}`);
+});
