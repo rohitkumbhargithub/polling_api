391af8b9c55ed9edd964e3adf12e247ddae5ae6e
Rohit Kumbhar <rohitkumbhar956@gmail.com>
2023-10-14 22:02:00 +0530

    commited

--- routes/api/options.js
+++ routes/api/options.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 const option_controller = require('../../controllers/option_controller');
 
-router.delete('/:id/delete', option_controller.delete);
-router.get('/:id/add_vote', option_controller.addVote);
+//this are the method
+//create the option 
 router.post('/:id/options/create', option_controller.create);
+//delete the option
+router.delete('/:id/delete', option_controller.delete);
+//add vote to the particular question
+router.get('/:id/add_vote', option_controller.addVote);
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//add the api index route
 console.log('Router Runing');
 router.use('/api', require('./api/index'));
 
